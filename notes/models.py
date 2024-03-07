6f5c8633e4552ed2520547c33e6a0b231cd976ad
Vlad <vladpoienariu@gmail.com>
2024-03-07 19:47:44 +0200

    a

--- notes/models.py
+++ notes/models.py
@@ -7,7 +7,7 @@ class Note(models.Model):
     title = models.CharField(max_length = 100, default=f"Note_{datetime.date.today}")
     content = models.TextField(blank=True)
     creationDate = models.DateTimeField(auto_now_add=True)
-    lastUpdate = models.IntegerField(blank=True, null=True, default = time.time() * 1000)
+    lastUpdate = models.IntegerField(blank=True, null=True, default = time.time() * 1000.0)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, null=True, blank=True)
     
     def __str__(self):

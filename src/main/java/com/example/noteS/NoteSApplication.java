3dc5adb4410409469a55cd07b20546fc25d38328
Vlad <vladpoienariu@gmail.com>
2025-04-01 13:14:38 +0300

    First init

---
+++ src/main/java/com/example/noteS/NoteSApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/noteS/NoteSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noteS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
